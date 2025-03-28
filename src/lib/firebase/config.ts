61ccab5ad874b2560e32e90620470ea64d3d86dd
Akshay Ramesh Nair <your.email@example.com>
2025-03-27 22:47:11 -0700

    chore: Force new build with latest config

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase app only if it hasn't been initialized already
+// initialize Firebase app only if it hasn't been initialized already
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
